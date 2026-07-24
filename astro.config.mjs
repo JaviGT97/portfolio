6b6fd24558a4eaaa8b0c5ff51398e4c240f6da22
Javier Garrido Terrón <javigt10@gmail.com>
2026-07-24 18:25:49 +0200

    Fix: eliminar workflows auto-generados, añadir .nojekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: "https://javigt97.github.io",
   base: "/portfolio",
+  build: {
+    assets: "assets",
+  },
 });
